e24018b95f8d28fac72799ec9fd38e587891dfff
TerrenceMM2 <terrencemm2@gmail.com>
2019-06-30 14:41:23 -0500

    validates letter.js functionality

--- letter.js
+++ letter.js
@@ -1,18 +1,27 @@
-function Letter(str) {
-    this.letter = str,
+// var guess = process.argv[2];
+
+function Letter(letter) {
+    this.letter = letter,
     this.guessed = false,
     this.placeholder = function() {
-        for (var i = 0; i < guessedLetters.length; i++) {
-            if (str === guessedLetters[i]) {
-                return guessedLetters[i];
+        for (var i = 0; i < 1; i++) {
+            if (letter === "x") {
+                return "x";
             } else {
                 return " _ ";
             };
         };
     },
-    this.beenGuessed = function(str) {
-        if (str) {
+    this.beenGuessed = function(letter) {
+        if (letter === this.letter) {
             this.guessed = true;
         };
     }
 };
+
+// var letter = new Letter(guess);
+
+// console.log(letter);
+// console.log(letter.placeholder());
+// console.log(letter.beenGuessed());
+// console.log(letter.guessed);
